f99f8d045c176d03be1e9499e2227a5acd49427d
HeatBlastee <mann42006@gmail.com>
2026-05-28 11:53:43 +0530

    testing2

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-// for new commit
+// for new commit   -new testing
 export function middleware(req: NextRequest) {
     const token = req.cookies.get("session")?.value;
 
